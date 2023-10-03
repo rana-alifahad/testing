27174d17674ce1ac1db796233c5c7d45d602fc41
unknown <alifahad.fsd.275@gmail.com>
2023-10-03 01:12:41 -0700

    first commit

---
+++ main.py
@@ -0,0 +1,13 @@
+# a =
+# b = [1, 2, 3, 4]
+# for i, j in zip(b, a):
+#     print(i, j)
+
+a = {
+    "students": ["fahran", "Ali", "Umair", "Asiya", "sadia"],
+    "roll_no": [1, 2, 3, 4, 5]
+}
+
+print(a["students"
+      ])
+
